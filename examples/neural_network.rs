d7ae719480e8cfb33e01f68dd08f9f46042a997e
Brendan Nolan <bnolan23@gmail.com>
2024-02-18 14:33:13 +0000

    Add Neural Network Example

---
+++ examples/neural_network.rs
@@ -0,0 +1,15 @@
+use crate::neural_network::*;
+use autodiff::*;
+use ndarray::*;
+
+fn sigmoid(x: F1) -> F1 {
+    F1::var(1.0) / (F1::var(1.0) + (-x).exp())
+}
+
+fn main() {
+    let network = NeuralNetworkBuilder::new(2, sigmoid)
+        .add_layer(array![[0.1, 0.2], [0.3, 0.4]], array![0.5, 0.6])
+        .add_layer(array![[0.7, 0.8]], array![0.9])
+        .build()
+        .unwrap();
+}
