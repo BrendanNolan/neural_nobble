407fcbf8afbddbb8386a7500e951ff08679348dc
Brendan Nolan <bnolan23@gmail.com>
2025-02-20 17:59:46 +0000

    In the cost func, normalise the cost by the number of examples

--- src/cost_functions.rs
+++ src/cost_functions.rs
@@ -14,12 +14,14 @@ pub struct HalfSSECostFunction;
 
 impl CostFunction for HalfSSECostFunction {
     fn cost(&self, final_layer_activation: &Array2<f64>, expected_activation: &Array2<f64>) -> f64 {
-        assert!(final_layer_activation.len() == expected_activation.len());
-        0.5_f64
+        let number_of_examples = final_layer_activation.len();
+        assert!(final_layer_activation.dim() == expected_activation.dim());
+        (1.0 / number_of_examples as f64)
+            * 0.5_f64
             * expected_activation
                 .iter()
                 .zip(final_layer_activation.iter())
-                .map(|(x, y)| (x + y).powi(2))
+                .map(|(x, y)| (x - y).powi(2))
                 .sum::<f64>()
     }
 
