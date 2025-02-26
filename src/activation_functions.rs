435e311b112bdec305d218821ca9df567a14afa8
Brendan Nolan <bnolan23@gmail.com>
2025-02-26 19:55:16 +0000

    Add the softmax activation function

--- src/activation_functions.rs
+++ src/activation_functions.rs
@@ -11,6 +11,7 @@ pub enum ActivationFunction {
     Id,
     Sigmoid,
     Relu,
+    SoftMax,
 }
 
 impl ActivationFunction {
@@ -27,6 +28,16 @@ impl ActivationFunction {
             ActivationFunction::Relu => {
                 let elems: Vec<_> = weighted_inputs.iter().map(|x| x.max(0.0)).collect();
                 Array2::<f64>::from_shape_vec(weighted_inputs.dim(), elems).unwrap()
+            }
+            ActivationFunction::SoftMax => {
+                let col_exp_sums: Vec<f64> = weighted_inputs
+                    .columns()
+                    .into_iter()
+                    .map(|col| col.iter().map(|x| x.exp()).sum())
+                    .collect();
+                Array2::<f64>::from_shape_fn(weighted_inputs.dim(), |(i, j)| {
+                    weighted_inputs[(i, j)].exp() * (1_f64 / col_exp_sums[j])
+                })
             }
         }
     }
@@ -45,6 +56,11 @@ impl ActivationFunction {
                     .collect();
                 Array2::<f64>::from_shape_vec(weighted_inputs.dim(), elems).unwrap()
             }
+            ActivationFunction::SoftMax => {
+                let softmax = self.apply(weighted_inputs);
+                let ones = Array2::<f64>::from_elem(weighted_inputs.dim(), 1_f64);
+                &softmax * (ones - &softmax)
+            }
         }
     }
 
@@ -55,6 +71,9 @@ impl ActivationFunction {
                 Normal::new(0.0, (1.0 / prev_layer_neuron_count as f64)).unwrap()
             }
             ActivationFunction::Relu => {
+                Normal::new(0.0, (2.0 / prev_layer_neuron_count as f64)).unwrap()
+            }
+            ActivationFunction::SoftMax => {
                 Normal::new(0.0, (2.0 / prev_layer_neuron_count as f64)).unwrap()
             }
         }
