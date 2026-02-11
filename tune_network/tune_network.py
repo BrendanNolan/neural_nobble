26071ea039ccdddee3ccf9929ffd1ef9daa936bd
Brendan Nolan <bnolan23@gmail.com>
2026-02-11 08:25:38 +0000

    Simplify the optuna criteria

--- tune_network/tune_network.py
+++ tune_network/tune_network.py
@@ -8,38 +8,29 @@ import time
 
 
 def objective(trial):
-    rng_seed = trial.suggest_int("rng_seed", 0, 100)
+    rng_seed = trial.suggest_int("rng_seed", 0, 1 << 32)
     loss = run_mnist(rng_seed)
     return loss
 
 
 def run_mnist(rng_seed):
     project_root = Path(__file__).resolve().parent.parent
-    start = time.time()
     proc_result = subprocess.run(["cargo", "run", "--release", "--example", "mnist", "--", f"{rng_seed}"],
         cwd=project_root,
         capture_output=True,
         text=True)
-    elapsed_seconds = time.time() - start
     if proc_result.returncode != 0:
         raise optuna.TrialPruned()
     output = proc_result.stdout
     hits = int(re.search(r"Hits: (\d+)", output).group(1))
-    misses = int(re.search(r"Misses: (\d+)", output).group(1))
     print()
-    print(f"Hits: {hits}, Misses: {misses}, Time: {elapsed_seconds} seconds")
-    miss_rate = misses / (hits + misses)
-    if miss_rate > 0.08:
-        raise optuna.TrialPruned()
-    if miss_rate == 0:
-        miss_rate = 1 / (1 << 16)
-    real_to_expected_seconds = elapsed_seconds / 3
-    return real_to_expected_seconds * miss_rate
+    print(f"Hits: {hits}")
+    return hits
 
 
 if __name__ == "__main__":
-    sample = optuna.create_study(direction="minimize")
-    sample.optimize(objective, n_trials=100)
+    sample = optuna.create_study(direction="maximize")
+    sample.optimize(objective, n_trials=500)
     print(sample.best_params)
     print(sample.best_value)
 
