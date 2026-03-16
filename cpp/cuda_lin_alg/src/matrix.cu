0dacae475493af3cff9ae2f756729dd39ddc4f72
Brendan Nolan <bnolan23@gmail.com>
2026-03-16 14:10:34 +0000

    Some more progress

--- cpp/cuda_lin_alg/src/matrix.cu
+++ cpp/cuda_lin_alg/src/matrix.cu
@@ -29,61 +29,101 @@ __device__ __forceinline__ bool access_legal(const MutableMatrixDetails& matrix,
     return access_legal(matrix.data, matrix.rows, matrix.columns, i, j, op);
 }
 
-__device__ __forceinline__ const float& at(const ConstMatrixDetails& matrix,
-        const unsigned int i,
-        const unsigned int j,
-        const Op op = Identity) {
-    assert(access_legal(matrix, i, j, op));
-    if (op == Transpose) {
-        return matrix.data[j * matrix.columns + i];
-    } else {
-        return matrix.data[i * matrix.columns + j];
-    }
-}
-
-__device__ __forceinline__ float& at(MutableMatrixDetails& matrix,
-        const unsigned int i,
-        const unsigned int j,
-        const Op op = Identity) {
-    assert(access_legal(matrix, i, j, op));
-    if (op == Transpose) {
-        return matrix.data[j * matrix.columns + i];
-    } else {
-        return matrix.data[i * matrix.columns + j];
-    }
-}
-
 struct Index {
     unsigned int i = 0u;
     unsigned int j = 0u;
 };
 
-__global__ void tiled_multiply(GemmParams params) {
+template <Op op>
+class GConstMatrixDetails {
+ public:
+    __device__ __forceinline__ GConstMatrixDetails(const ConstMatrixDetails* inner)
+        : inner_{inner} {
+    }
+    __device__ __forceinline__ const float& at(const unsigned int i, const unsigned int j) const {
+        if constexpr (op == Transpose) {
+            return inner_->data[j * inner_->columns + j];
+        } else {
+            return inner_->data[i * inner_->columns + j];
+        }
+    }
+    __device__ __forceinline__ unsigned int rows() const {
+        if constexpr (op == Transpose) {
+            return inner_->columns;
+        } else {
+            return inner_->rows;
+        }
+    }
+    __device__ __forceinline__ unsigned int columns() const {
+        if constexpr (op == Transpose) {
+            return inner_->rows;
+        } else {
+            return inner_->columns;
+        }
+    }
+ private:
+    const ConstMatrixDetails* inner_ = nullptr;
+};
+
+template <Op op>
+class GMutableMatrixDetails {
+ public:
+    __device__ __forceinline__ GMutableMatrixDetails(MutableMatrixDetails* inner)
+        : inner_{inner} {
+    }
+    __device__ __forceinline__ float& at(const unsigned int i, const unsigned int j) {
+        if constexpr (op == Transpose) {
+            return inner_->data[j * inner_->columns + j];
+        } else {
+            return inner_->data[i * inner_->columns + j];
+        }
+    }
+    __device__ __forceinline__ unsigned int rows() const {
+        if constexpr (op == Transpose) {
+            return inner_->columns;
+        } else {
+            return inner_->rows;
+        }
+    }
+    __device__ __forceinline__ unsigned int columns() const {
+        if constexpr (op == Transpose) {
+            return inner_->rows;
+        } else {
+            return inner_->columns;
+        }
+    }
+ private:
+    MutableMatrixDetails* inner_ = nullptr;
+};
+
+template <Op op_A, Op op_B>
+struct GGemmParams {
+    GConstMatrixDetails<op_A> A;
+    float alpha;
+    GConstMatrixDetails<op_B> B;
+    float beta;
+    float* C;
+};
+
+// Deals with GConstMatrixDetails and GMutableMatrixDetails, sometimes for the performance
+// optimisation of knowing the op at compile time, sometimes for the convenience of having the .at()
+// method, which is not available on the ConstMatrixDetails and MutableMatrixDetails types since
+// they must be extern C.
+template <Op op_A, Op op_B>
+__global__ void tiled_multiply(GGemmParams<op_A, op_B> params) {
     assert(blockDim.y == ratio_block_y_to_block_x * blockDim.x);
     assert(square_root_of_ratio_block_y_to_block_x * square_root_of_ratio_block_y_to_block_x
             == ratio_block_y_to_block_x);
     extern __shared__ float shared[];
-    auto a_at = [params](unsigned int i, unsigned int j) -> float {
-        return at(params.A, i, j, params.op_A);
-    };
-    auto b_at = [params](unsigned int i, unsigned int j) -> float {
-        return at(params.B, i, j, params.op_B);
-    };
-    if (params.op_A == Transpose) {
-        cuda_helpers::swap(params.A.rows, params.A.columns);
-    }
-    if (params.op_B == Transpose) {
-        cuda_helpers::swap(params.B.rows, params.B.columns);
-    }
-    const auto ai = params.A.rows;
-    const auto aj = params.A.columns;
-    const auto bi = params.B.rows;
-    const auto bj = params.B.columns;
-    auto C = MutableMatrixDetails{.data = params.C, .rows = ai, .columns = bj};
+    auto c_inner = MutableMatrixDetails{
+            .data = params.C, .rows = params.A.rows(), .columns = params.B.columns()};
+    auto C = GMutableMatrixDetails<Identity>{&c_inner};
     const auto T = blockDim.y;
     auto final_c_value = 0.0f;
-    auto a_tile = MutableMatrixDetails{.data = shared, .rows = T, .columns = T};
-    auto b_tile = MutableMatrixDetails{.data = a_tile.data + T * T, .rows = T, .columns = T};
+    auto a_tile_inner = MutableMatrixDetails{.data = shared, .rows = T, .columns = T};
+    auto a_tile = GMutableMatrixDetails<Identity>{&a_tile_inner};
+    auto b_tile_inner = MutableMatrixDetails{.data = a_tile.data + T * T, .rows = T, .columns = T};
+    auto b_tile = GMutableMatrixDetails<Identity>{&b_tile_inner};
     // Remember that cuda indexes grid/block rows with y and columns with x, like the x and y axes
     // of a graph, not the rows and columns of a matrix.
     for (auto row = 0u; row < ai; row += gridDim.y * blockDim.y) {
@@ -101,20 +141,27 @@ __global__ void tiled_multiply(GemmParams params) {
             auto final_c_values = MutableMatrixDetails{.data = final_data,
                     .rows = square_root_of_ratio_block_y_to_block_x,
                     .columns = square_root_of_ratio_block_y_to_block_x};
-
             for (auto k = 0u; k < aj; k += T) {
-                a_tile[tile_slot] = in_scope_for_a ? a_at(g_i, k + threadIdx.x) : 0u;
-                b_tile[tile_slot] = in_scope_for_b ? b_at(k + threadIdx.y, g_j) : 0u;
-                __syncthreads();
-                for (auto mini_i = 0u; mini_i < square_root_of_ratio_block_y_to_block_x; ++mini_i) {
-                    for (auto mini_j = 0u; mini_j < square_root_of_ratio_block_y_to_block_x;
-                            ++mini_j) {
+                for (auto kk = 0u; kk < bj; kk += T) {
+                    for (auto mini_i = 0u; mini_i < square_root_of_ratio_block_y_to_block_x;
+                            ++mini_i) {
+                        for (auto mini_j = 0u; mini_j < square_root_of_ratio_block_y_to_block_x;
+                                ++mini_j) {
+                            a_tile[tile_slot] = in_scope_for_a ? a_at(g_i, k + threadIdx.x) : 0u;
+                            b_tile[tile_slot] = in_scope_for_b ? b_at(k + threadIdx.y, g_j) : 0u;
+                        }
                         __syncthreads();
+                        for (auto mini_i = 0u; mini_i < square_root_of_ratio_block_y_to_block_x;
+                                ++mini_i) {
+                            for (auto mini_j = 0u; mini_j < square_root_of_ratio_block_y_to_block_x;
+                                    ++mini_j) {
+                                __syncthreads();
+                            }
+                        }
                     }
                 }
+                // Write back to main memory
             }
-            if (c_global_index_valid)
-                params.C[c_global_index] = params.alpha * final_c_value;
         }
     }
 }
